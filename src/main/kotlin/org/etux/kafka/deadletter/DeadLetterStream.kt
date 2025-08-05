7a58f89baf021729248bedcc40d4734bf18bc8ad
Blagoja Stamatovski <95382055+call-me-baki@users.noreply.github.com>
2025-08-05 10:24:59 +0200

    More changes

--- src/main/kotlin/org/etux/kafka/deadletter/DeadLetterStream.kt
+++ src/main/kotlin/org/etux/kafka/deadletter/DeadLetterStream.kt
@@ -1,18 +1,16 @@
 package org.etux.kafka.deadletter
 
-import org.apache.kafka.common.serialization.Deserializer
-import org.apache.kafka.common.serialization.Serde
 import org.apache.kafka.common.serialization.Serdes
-import org.apache.kafka.common.serialization.Serializer
 import org.apache.kafka.streams.KafkaStreams
 import org.apache.kafka.streams.StreamsBuilder
 import org.apache.kafka.streams.Topology
 import org.apache.kafka.streams.kstream.Consumed
 import org.apache.kafka.streams.kstream.KStream
-import org.apache.kafka.streams.kstream.Materialized
+import org.apache.kafka.streams.state.KeyValueStore
+import org.apache.kafka.streams.state.StoreBuilder
 import org.apache.kafka.streams.state.Stores
-import org.etux.kafka.deadletter.serdes.DeadLetterJacksonSerde
-import org.etux.kafka.deadletter.serdes.DeadLetterListJacksonSerde
+import org.etux.kafka.ExampleKafkaMessage
+import org.etux.kafka.serdes.DeadLetterMessageSerde
 import org.slf4j.LoggerFactory
 import java.util.Properties
 
@@ -26,6 +24,14 @@ class DeadLetterStream(
 
     private fun topology(): Topology {
         val builder = StreamsBuilder()
+
+        val storeBuilder: StoreBuilder<KeyValueStore<String, DeadLetterMessage>> = Stores.keyValueStoreBuilder(
+            /* supplier = */ Stores.inMemoryKeyValueStore(inputStore),
+            /* keySerde = */ Serdes.String(),
+            /* valueSerde = */ DeadLetterMessageSerde(),
+        )
+        builder.addStateStore(storeBuilder)
+
         val deadLetterStream: KStream<String, String> = builder.stream(
             /* topic = */ inputTopic,
             /* consumed = */ Consumed.with(
@@ -35,35 +41,17 @@ class DeadLetterStream(
         )
 
         deadLetterStream
+            .peek { key, value -> logger.info("Received message. Key: $key, Value: $value") }
             .filter { _, value ->
-                when(ExampleKafkaApplication.MessageType.valueOf(value)) {
-                    ExampleKafkaApplication.MessageType.SUCCESSFUL, ExampleKafkaApplication.MessageType.RETRY -> true
+                when (ExampleKafkaMessage.MessageType.valueOf(value)) {
+                    ExampleKafkaMessage.MessageType.SUCCESSFUL, ExampleKafkaMessage.MessageType.RETRY -> true
                     else -> false
                 }
-            }
-
-        // Group by key and aggregate into a list per key
-        deadLetterStream
-            .peek { key, value -> logger.info("Received message. Key: $key, Value: $value") }
-            .groupByKey()
-            .aggregate(
-                /* p0 = */ { emptyList() },
-                /* p1 = */ { key: String, value: String, aggregate: List<String> ->
-                    logger.info("Adding to dead letter messages bucket $key of size ${aggregate.size} record: $value")
-                    aggregate + value
-                },
-                /* p2 = */ Materialized.`as`<String, List<String>>(Stores.inMemoryKeyValueStore(inputStore))
-                    .withKeySerde(Serdes.String())
-                    .withValueSerde(object: Serde<List<String>> { // TODO extract serde
-                        override fun serializer() = Serializer<List<String>> { _, data ->
-                            data?.joinToString(separator = ",")?.toByteArray()
-                        }
-                        override fun deserializer() = Deserializer { _, bytes ->
-
-                            bytes?.toString(Charsets.UTF_8)?.split(",") ?: emptyList()
-                        }
-                    }),
+            }.process(
+                /* processorSupplier = */ DeadLetterProcessorSupplier(inputStore),
+                /* ...stateStoreNames = */ inputStore,
             )
+            .groupByKey()
 
         return builder.build()
     }
