0dc788db3718ddce876f5fbfbf4a574944a1b5e6
Blagoja Stamatovski <95382055+call-me-baki@users.noreply.github.com>
2025-08-06 17:57:48 +0200

    More nice things

--- src/main/kotlin/org/etux/kafka/deadletter/DeadLetteredValue.kt
+++ src/main/kotlin/org/etux/kafka/deadletter/DeadLetteredValue.kt
@@ -1,9 +1,13 @@
 package org.etux.kafka.deadletter
 
+import java.io.Serializable
 import java.util.UUID
 
 data class DeadLetteredValue<V>(
     val retryCount: Int,
     val uniqueMessageId: UUID,
     val payload: V,
-)
+    val cause: String? = null,
+    val stackTrace: String? = null,
+    val headers: Map<String, String> = emptyMap(),
+): Serializable
